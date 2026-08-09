44b6485df0bc6d59f9eae3490272ed2df997f981
Franc02306 <francosm230602@gmail.com>
2026-08-08 19:42:12 -0500

    feat: nuevo posicionamiento de componentes, implementacion de pantalla COMO JUGAR y de boton SALIR

---
+++ HowToPlay.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class HowToPlay : Control
+{
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is not InputEventKey keyEvent)
+			return;
+
+		if (!keyEvent.Pressed || keyEvent.Echo)
+			return;
+
+		if (keyEvent.Keycode == Key.Escape)
+		{
+			GetTree().ChangeSceneToFile("res://MainMenu.tscn");
+		}
+	}
+}
--- MainMenu.cs
+++ MainMenu.cs
@@ -11,11 +11,15 @@ public partial class MainMenu : Control
 {
 	public override void _Ready()
 	{
-		GetNode<Button>("DifficultyContainer/SmallButton").Pressed += () => StartGame(9, 9, 10);
+		GetNode<Button>("MenuCenter/DifficultyContainer/SmallButton").Pressed += () => StartGame(9, 9, 10);
 
-		GetNode<Button>("DifficultyContainer/MediumButton").Pressed += () => StartGame(16, 16, 40);
+		GetNode<Button>("MenuCenter/DifficultyContainer/MediumButton").Pressed += () => StartGame(16, 16, 40);
 
-		GetNode<Button>("DifficultyContainer/LargeButton").Pressed += () => StartGame(16, 30, 99);
+		GetNode<Button>("MenuCenter/DifficultyContainer/LargeButton").Pressed += () => StartGame(16, 30, 99);
+		
+		GetNode<Button>("MenuCenter/DifficultyContainer/HowToPlayButton").Pressed += OpenHowToPlay;
+		
+		GetNode<Button>("MenuCenter/DifficultyContainer/ExitButton").Pressed += ExitGame;
 	}
 
 	private void StartGame(int rows, int columns, int mines)
@@ -25,5 +29,15 @@ public partial class MainMenu : Control
 		GameSettings.Mines = mines;
 
 		GetTree().ChangeSceneToFile("res://Game.tscn");
+	}
+	
+	private void OpenHowToPlay()
+	{
+		GetTree().ChangeSceneToFile("res://HowToPlay.tscn");
+	}
+	
+	private void ExitGame()
+	{
+		GetTree().Quit();
 	}
 }
